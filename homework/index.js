4ae0575888c6b0527b94a45c24a86eb7218f4d84
steevjunior <bitomagira@yahoo.fr>
2019-03-21 21:00:49 +0100

    after the first modifications suggested by Vincent

--- homework/index.js
+++ homework/index.js
@@ -1,16 +1,13 @@
 function getPromise() {
-  const ret = fetch('https://api.github.com/orgs/HackYourFuture/repos?per_page=100')
+  return fetch('https://api.github.com/orgs/HackYourFuture/repos?per_page=100')
     .then(response => response.json());
-  return ret;
 }
-
-
-function populate(Prom) {
-  const selectContent = document.getElementById('liste').value;
-  Prom.then((result) => {
+const myPromise = getPromise();
+function selectedRepoCaracteristics(selectValue) {
+  myPromise.then((result) => {
     result.forEach((x) => {
-      if (x.name === selectContent) {
-        document.getElementById('show').innerHTML = `
+      if (x.name === selectValue) {
+        document.getElementById('repoCaracteristics').innerHTML = `
          <b>Repository :</b>   <a href="">${x.name} </a><br>
           <b>Descripion :</b>   ${x.description} <br>
           <b>Forks :</b>   ${x.forks} <br>
@@ -19,46 +16,25 @@ function populate(Prom) {
     });
   });
 }
-
-
-function loadPage(Prom) {
-  Prom.then((result) => {
-    const nameArray = [];
-    // make an array of all the names
-
-    result.forEach((item) => {
-      nameArray.push(item.name);
-    });
-    //  populate the select with the elements in the array
-
+function loadPage() {
+  myPromise.then((result) => {
+    // Make an array of all the names
+    const nameArray = result.map(item => item.name);
+    // Fill the select with the elements in the array
     nameArray.forEach((x) => {
-      const node = document.createElement('OPTION');
+      const node = document.createElement('option');
       const textnode = document.createTextNode(x);
       node.appendChild(textnode);
-      document.getElementById('liste').appendChild(node);
+      document.getElementById('mySelect').appendChild(node);
     });
-
-    // populate the div according to the selected item
-    // this code is here to ensure the caracteristics of the default value are printed
-    // after this action will be handled by the populate function
-    // when the value of the section is changed
-
-    const selectContent = document.getElementById('liste').value;
-
-    result.forEach((x) => {
-      if (x.name === selectContent) {
-        document.getElementById('show').innerHTML = `
-          <b>Repository :</b>   <a href="">${x.name} </a><br>
-          <b>Descripion :</b>   ${x.description} <br>
-          <b>Forks :</b>   ${x.forks} <br>
-          <b>Updated :</b>   ${new Date(x.updated_at).toLocaleString('en-US')} <br>
-          `;
-      }
-    });
+    // Show the caracteristics of the default value of the select
+    const selectValue = document.getElementById('mySelect').value;
+    selectedRepoCaracteristics(selectValue);
   });
 }
-const myPromise = getPromise();
-loadPage(myPromise);
-document.getElementById('liste').addEventListener('change', () => {
-  populate(myPromise);
+
+loadPage();
+document.getElementById('mySelect').addEventListener('change', () => {
+  // eslint-disable-next-line no-undef
+  selectedRepoCaracteristics(mySelect.value);
 });
