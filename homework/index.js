c75814383430d54b1c763661a75d0088341bd22a
GitHub <zilhan.be@gmail.com>
2019-03-25 14:25:19 +0100

    almost done

--- homework/index.js
+++ homework/index.js
@@ -24,9 +24,31 @@ window.onload = () => fetch('https://api.github.com/orgs/HackYourFuture/repos?pe
       document.getElementById('repo-forks').innerHTML = data[selectedRepoIndex - 1].forks;
       const time = new Date(data[selectedRepoIndex - 1].updated_at);
       document.getElementById('repo-updated').innerHTML = time.toUTCString();
+      fetch(data[selectedRepoIndex - 1].contributors_url)
+        .then(status)
+        .then(response => response.json())
+        .then((data) => {
+          document.getElementById('snipper').style.visibility = 'visible';
+          const contributors = data;
+          const mylist = document.getElementById('contributor-list');
+          const lis = mylist.getElementsByTagName('li');
+          while (lis.length > 0) {
+            mylist.removeChild(lis[0]);
+          }
+          contributors.forEach((person) => {
+            const listItem = document.createElement('li');
+            listItem.setAttribute('class', 'contributor-item');
+            listItem.setAttribute('aria-label', person.login);
+            listItem.innerHTML = `<img src="${person.avatar_url}" width="50" height="50" alt="avatar"><div class="contributor-data"><div>${person.login}</div><div class="contributor-badge">${person.contributions}</div></div>`;
+            document.getElementById('contributor-list').appendChild(listItem);
+          });
+        })
+        .catch((error) => {
+          console.log('Request failed', error);
+        })
+        .then(() => {
+          document.getElementById('snipper').style.visibility = 'hidden';
+        });
     };
-  })
-  .catch((error) => {
-    // eslint-disable-next-line no-console
-    console.log('Request failed', error);
   });
+
