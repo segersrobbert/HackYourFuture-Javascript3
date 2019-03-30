a970f955c3755fee649f6b6dc92ed079e1fb997e
Prasamsha Sharma <prasamsha.sharma@yahoo.com>
2019-03-30 16:35:53 +0100

    some changes

--- homework/index.js
+++ homework/index.js
@@ -3,19 +3,26 @@ const unContainer = document.querySelector('.right__div-username');
 const reposContainer = document.querySelector('.right__div-repos');
 const avatarContainer = document.querySelector('.right__div-avatar');
 
-
 const CONTRIBUTORS_URL = async () => {
   const API_CALL = await fetch(
     'https://api.github.com/repos/HackYourFuture/tdd-game/contributors',
   );
   const data = await API_CALL.json();
-  return { data };
+  return {
+    data,
+  };
 };
 const showContributorsDetails = () => {
   CONTRIBUTORS_URL(repoSelectElement.value).then((response) => {
-    unContainer.innerHTML = `username:<span class="right_div-value">${response.data.login}</span>`;
-    reposContainer.innerHTML = `Repos:<span class="right_div-value"> ${response.data.repos_url}</span>`;
-    avatarContainer.innerHTML = `<span class="right_div-value">${response.data.avatar_url}</span>`;
+    unContainer.innerHTML = `username:<span class="right_div-value">${
+      response.data.login
+    }</span>`;
+    reposContainer.innerHTML = `Repos:<span class="right_div-value"> ${
+      response.data.repos_url
+    }</span>`;
+    avatarContainer.innerHTML = `<span class="right_div-value">${
+      response.data.avatar_url
+    }</span>`;
   });
 };
 showContributorsDetails();
--- homework/index1.js
+++ homework/index1.js
@@ -1,9 +1,9 @@
-const REPOSITORIES_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
-// const CONTRIBUTORS_URL = 'https://api.github.com/repos/HackYourFuture/tdd-game/contributors';
-let repositories;
+const unContainer = document.querySelector('.username');
+const reposContainer = document.querySelector('.repos');
+const avatarContainer = document.querySelector('.avatar');
 const repoSelectElement = document.getElementById('repositories');
-// const contributorElement = document.getElementById('contributorElements');
-
+const REPOSITORIES_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+let repositories;
 
 function populateSelectList() {
   repositories.forEach((r) => {
@@ -23,48 +23,47 @@ function showSelectedRepoDetails() {
   const selectedIndex = repoSelectElement.selectedIndex - 1;
 
   if (selectedIndex < 0) return; // Don't do anything upon selecting the 'Please choose...' option
-
   const selectedRepo = repositories[selectedIndex];
-
-
   document.getElementById('repo-name').innerHTML = selectedRepo.name;
-
   document.getElementById('repo-description').innerHTML = selectedRepo.description;
-
   document.getElementById('repo-forks').innerHTML = selectedRepo.forks;
-
   const updateDate = new Date(selectedRepo.updated_at);
-
   const date = updateDate.toLocaleDateString();
-
   const time = updateDate.toLocaleTimeString();
-
   document.getElementById('repo-updated').innerHTML = `${date} ${time}`;
 }
-
-/* const showContributorsDetails = () => {
-  CONTRIBUTORS_URL().then((response) => {
-    contributorElement.innerHTML = `username:${response.data.login}`;
+const getContributors = async () => {
+  const API_CALL = await fetch(
+    'https://api.github.com/repos/HackYourFuture/tdd-game/contributors',
+  );
+  const data = await API_CALL.json();
+  return {
+    selectedIndex: data,
+  };
+};
+const showContributorsDetails = () => {
+  getContributors(getContributors.value).then((response) => {
+    unContainer.innerHTML = `username:<span class="right_div-value">${
+      response.data.login
+    }</span>`;
+    reposContainer.innerHTML = `Repos:<span class="right_div-value"> ${
+      response.data.repos_url
+    }</span>`;
+    avatarContainer.innerHTML = `<span class="right_div-value">${
+      response.data.avatar_url
+    }</span>`;
   });
-}; */
-
+};
 window.onload = () => {
   fetch(REPOSITORIES_URL)
     .then(response => response.json())
 
     .then((data) => {
       repositories = data;
-
-      /* fetch(CONTRIBUTORS_URL)
-        .then(response => response.json()); */
-
-
       populateSelectList();
 
       showSelectedRepoDetails();
-      //  showContributorsDetails();
+      showContributorsDetails();
     });
 };
-
-
 repoSelectElement.onchange = showSelectedRepoDetails;
